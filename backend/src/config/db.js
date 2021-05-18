2725b4d656b3209c167524257110a7022e5b838b
Henry Leão <48421488+henrylsilveira@users.noreply.github.com>
2021-05-17 22:15:27 -0300

    Delete backend directory

--- backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-
-module.exports = new Pool ({
-    user: 'postgres',
-    password: '919364',
-    host: "localhost",
-    port: 5432,
-    database: "petshopflutter"
-});
